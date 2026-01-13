0e1e5771e54222efbe6960192bc515d28c3a20d2
tomerli <tomerli@users.noreply.github.com>
2026-01-13 19:15:49 +0000

    deploy: 30f6fcf3ffcec2b4e3240bc4cffda5d905c68bbe

--- functions.js
+++ functions.js
@@ -6,5 +6,5 @@ function func_i651ug() { return "Motivation: You can do it!"; }
 function func_b3pvoe() { return "Fruit: Orange"; }
 function func_27kvvl() { return "Fruit: Orange"; }
 function func_omuoxq() { return "Current seconds: 53"; }
-function func_nqouci() { return "Quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.'"; }
+function func_xk2wxo() { return "Quote: 'The only way to do great work is to love what you do.'"; }
 function func_vv1iv8() { return "Current seconds: 5"; }
